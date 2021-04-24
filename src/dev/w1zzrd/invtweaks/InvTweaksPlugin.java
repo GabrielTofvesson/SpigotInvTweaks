23ee65ed40c3953afd94c5aa9494e38c6164517d
Gabriel Tofvesson <gabto095@student.liu.se>
2021-04-24 02:11:35 +0200

    Add /sort event listener

--- src/dev/w1zzrd/invtweaks/InvTweaksPlugin.java
+++ src/dev/w1zzrd/invtweaks/InvTweaksPlugin.java
@@ -1,9 +1,11 @@
 package dev.w1zzrd.invtweaks;
 
 import dev.w1zzrd.invtweaks.command.SortCommandExecutor;
+import dev.w1zzrd.invtweaks.listener.SortListener;
 import dev.w1zzrd.invtweaks.listener.StackReplaceListener;
 import dev.w1zzrd.logging.LoggerFactory;
 import org.bukkit.event.HandlerList;
+import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import java.util.Objects;
@@ -20,7 +22,10 @@ public class InvTweaksPlugin extends JavaPlugin {
 
         Objects.requireNonNull(getCommand("sort")).setExecutor(new SortCommandExecutor());
 
-        getServer().getPluginManager().registerEvents(new StackReplaceListener(), this);
+        final PluginManager pluginManager = getServer().getPluginManager();
+
+        pluginManager.registerEvents(new StackReplaceListener(), this);
+        pluginManager.registerEvents(new SortListener(), this);
     }
 
     @Override
