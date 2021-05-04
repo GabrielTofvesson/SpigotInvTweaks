0e8ab30672cb71f137aed2ad378422542fc81a62
Gabriel Tofvesson <gabto095@student.liu.se>
2021-05-04 05:54:18 +0200

    Implement search configuration type

---
+++ src/dev/w1zzrd/invtweaks/serialization/SearchConfig.java
@@ -0,0 +1,29 @@
+package dev.w1zzrd.invtweaks.serialization;
+
+import java.util.Map;
+
+public class SearchConfig extends SimpleReflectiveConfigItem {
+
+    private int searchRadiusX, searchRadiusY, searchRadiusZ;
+
+    /**
+     * Required constructor for deserializing data
+     *
+     * @param mappings Data to deserialize
+     */
+    public SearchConfig(Map<String, Object> mappings) {
+        super(mappings);
+    }
+
+    public int getSearchRadiusX() {
+        return searchRadiusX;
+    }
+
+    public int getSearchRadiusY() {
+        return searchRadiusY;
+    }
+
+    public int getSearchRadiusZ() {
+        return searchRadiusZ;
+    }
+}
